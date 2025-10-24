2f16fee555060909befaa839f98fcdecbaed2366
achyutkneupane <achyutkneupane@gmail.com>
2025-10-25 01:05:14 +0545

    feat: implements HamroCDNContract in HamroCDN class & returns dummy data

--- src/HamroCDN.php
+++ src/HamroCDN.php
@@ -4,13 +4,34 @@ declare(strict_types=1);
 
 namespace HamroCDN;
 
+use HamroCDN\Contracts\HamroCDNContract;
+
 /**
  * @phpstan-type HamroCDNObject array{
  *     id: string,
  *     nanoId: string,
  * }
  */
-final class HamroCDN
+final class HamroCDN implements HamroCDNContract
 {
-    //
+    public function upload(string $filePath): array
+    {
+        return [
+            'id' => 'sample_id',
+            'nanoId' => 'sample_nano_id',
+        ];
+    }
+
+    public function fetch(string $id): array
+    {
+        return [
+            'id' => 'sample_id',
+            'nanoId' => 'sample_nano_id',
+        ];
+    }
+
+    public function delete(string $id): bool
+    {
+        return true;
+    }
 }
